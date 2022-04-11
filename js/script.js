c82217f94b474718bfc57dd3924e5b27b3783d9b
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-04-11 17:00:56 +0300

    addEventListener - scroll , getBoundingClientRect()

---
+++ js/script.js
@@ -0,0 +1,69 @@
+
+//================================================================
+// old-school
+
+// const hikeExp = document.querySelector(".hike-exp");
+// window.addEventListener("scroll", scrollReveal);
+// function scrollReveal(){
+//     console.log(hikeExp);
+//     console.log(111);
+// }
+
+//================================================================
+// another way - getBoundingClientRect()
+
+// const hikeExp = document.querySelector(".hike-exp");
+// window.addEventListener("scroll", scrollReveal);
+// - //-1
+// function scrollReveal(){ 
+//     const hikePos = hikeExp.getBoundingClientRect();
+//     console.log(hikePos);
+//     // what I get:
+//     // DOMRect {x: 641.3515625, y: 1253, width: 126.0703125, height: 53.5, top: 1253, …}
+//     // bottom: 1306.5
+//     // height: 53.5
+//     // left: 641.3515625
+//     // right: 767.421875
+//     // top: 1253
+//     // width: 126.0703125
+//     // x: 641.3515625
+//     // y: 1253
+// }
+
+// - //-2
+// function scrollReveal(){ 
+//     const hikePos = hikeExp.getBoundingClientRect().top;
+//     console.log(hikePos);
+//     // what I get: 1290 ... and so on ... to zero and below
+//     if(hikePos < 0){
+//         alert("hello")
+//     }
+// }
+
+// - //-3
+// function scrollReveal(){ 
+//     const hikePos = hikeExp.getBoundingClientRect().top;
+//     const windowHeight = window.innerHeight
+//     console.log(windowHeight);
+//     // what I get:  600 px; or any other height of a window
+
+//     if(hikePos < windowHeight){
+//         console.log("hello");
+//     }
+// }
+
+// - //-4
+// function scrollReveal(){ 
+//     const hikePos = hikeExp.getBoundingClientRect().top;
+//     const windowHeight = window.innerHeight / 1.5;
+//     console.log(windowHeight);
+//     // what I get:  600 px; or any other height of a window
+
+//     if(hikePos < windowHeight){
+//         console.log("hello");
+//         hikeExp.style.color = 'red';
+//     }
+// }
+
+
+
